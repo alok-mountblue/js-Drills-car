13ed24077d51e8ebfa670a31aacb8bb109a002d4
alok <alok.sharma.17@mountblue.tech>
2021-08-10 23:00:43 +0530

    problem3 solved

---
+++ problem3.js
@@ -0,0 +1,25 @@
+
+export let problem3=function problem3(inventory){
+
+   let carModels=[];
+   for (let i = 0; i < inventory.length; i++) 
+     carModels[i] = inventory[i].car_model.toLocaleLowerCase();
+
+    var i = 0;
+    var j;
+    while (i < carModels.length) {
+        j = i + 1;
+      while (j < carModels.length) {
+          if (carModels[j] < carModels[i]) {
+             var tempStr = carModels[i];
+             carModels[i] = carModels[j];
+             carModels[j] = tempStr;
+            }
+            j++;
+         }
+     i++;
+   }
+
+   return carModels;
+   
+}
---
+++ test/testProblem3.js
@@ -0,0 +1,8 @@
+// ==== Problem #3 ====
+// The marketing team wants the car models listed alphabetically on the website. Execute a function to Sort all the car model names into alphabetical order and log the results in the console as it was returned.
+
+import {inventory} from "../array.js";
+import {problem3} from "../problem3.js";
+
+var result=problem3(inventory)
+console.log(result);
